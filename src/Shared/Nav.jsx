56273ccad7da7873f4c167366e4fcdb0e33761f8
Source-Code-007 <utsho926@gmail.com>
2023-05-17 10:14:38 +0600

    navbar complete

--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../assets/logo.png'
 
 const Nav = () => {
+        const menu = <>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+            <li><Link to={'/volunteer'}>Volunteer</Link></li>
+        </>
     return (
-        <div className='bg-slate-600 py-11'>
-            This is Nav
+
+        <div className="navbar bg-teal-500">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
+                      {menu}
+                    </ul>
+                </div>
+                <Link to={'/'}><img className="w-24 h-auto" src={logo} alt="" /></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex font-semibold">
+                <ul className="menu menu-horizontal px-1">
+                    {menu}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
+            </div>
         </div>
     );
 };
